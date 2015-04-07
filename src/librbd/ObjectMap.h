rlrevell/ceph

librbd: correct basic object map errors during refresh
If the object map is corrupt on-disk or too small for the image,
correct these basic issues as soon as possible.  The object map
is still flagged as invalid, but there will be less required repair
work if future IO is able to properly update the object map.

Signed-off-by: Jason Dillaman <dillaman@redhat.com>

src/librbd/ObjectMap.h
@@ -119,13 +119,11 @@ private:
   };
 
   ImageCtx &m_image_ctx;
-
   ceph::BitVector<2> m_object_map;
-
   bool m_enabled;
 
   void invalidate();
-
+  void resize(uint64_t num_objs, uint8_t defualt_state);
 };
 
 } // namespace librbd

prallabh/ceph-1

src/librbd/ObjectMap.h
@@ -123,6 +123,7 @@ private:
   bool m_enabled;
 
   void invalidate(uint64_t snap_id);
+  void resize(uint64_t num_objs, uint8_t default_state);
 };
 
 } // namespace librbd